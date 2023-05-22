a2f23c5462cab69770d168e358a16f243543a067
DavidMRGaona <23616974+DavidMRGaona@users.noreply.github.com>
2023-05-22 14:36:41 +0200

    CookieHub now uses API v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
             // The ID is part of the CookieHub URL: https://cookiehub.net/cc/YOUR_COOKIEHUB_ID.js
             cookieHubId: process.env.COOKIEHUB_BANNER_ID,
             // Optional parameter (default false) - Use new v2 API.
-            cookieHubV2Api: false,
+            cookieHubV2Api: true,
             // Categories configured with CookieHub
             categories: [
             {
